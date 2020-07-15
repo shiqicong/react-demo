92aab98c7a90f117e98240a3535ca734cc6dc256
shiqicong <shiqicong@mininglamp.com>
2020-07-15 16:45:53 +0800

    base

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,12 @@
 import React,{useState,useEffect,useCallback,useContext} from 'react';
-
+import {NavLink} from "react-router-dom"
 import './scss/index.scss'
 function Header(){
 
     return(
         <div className='header'>
-            header
+            {/* header */}
+            <NavLink to='/home'>3d</NavLink>
         </div>
     )
 }
